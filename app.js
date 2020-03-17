dd74fa85763acaec64f358d961ae517b975a3576
sabatino <sabatogalasso90@gmail.com>
2020-03-17 19:25:38 +0100

    test

--- app.js
+++ app.js
@@ -31,11 +31,11 @@ app.use(customersRouter);
 
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/managerAppFrontend'));
+/*app.use(express.static(__dirname + '/dist/managerAppFrontend'));
 
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/managerAppFrontend/index.html'));
-});
+});*/
 
 
 app.listen(port, () => {
